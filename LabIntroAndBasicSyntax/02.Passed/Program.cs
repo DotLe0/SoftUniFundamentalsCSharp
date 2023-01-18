5d18f6a9d2afbf383d56e7abae916b7a73e12e70
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-18 16:52:40 +0200

    Adding

---
+++ LabIntroAndBasicSyntax/02.Passed/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _02.Passed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grande = double.Parse(Console.ReadLine());
+
+            if (grande >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
