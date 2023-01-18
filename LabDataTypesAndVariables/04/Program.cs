d68f2f210f5daa82a900275f855c16a0d18a47c5
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-18 20:31:45 +0200

    Ex4 Updated and ex5 added

--- LabDataTypesAndVariables/04/Program.cs
+++ LabDataTypesAndVariables/04/Program.cs
@@ -9,7 +9,7 @@
             int days = (int)(years * 365.2422);
             int hours = days * 24;
             int minutes = hours * 60;
-            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {minutes} minutes");
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
         }
     }
 }
---
+++ LabDataTypesAndVariables/05/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int currentNum = i;
+                int sum = 0;
+
+                while (currentNum > 0)
+                {
+                    sum += currentNum % 10;
+                    currentNum = currentNum / 10;
+                }
+
+                if (sum == 5 || sum == 7 || sum ==11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+        }
+    }
+}
