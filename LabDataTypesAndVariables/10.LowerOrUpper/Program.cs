4f28e748ee9d32d40df1223cb0fa8ec402c50340
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-19 19:57:48 +0200

    adding ex 10

---
+++ LabDataTypesAndVariables/10.LowerOrUpper/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _10.LowerOrUpper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char c1 = char.Parse(Console.ReadLine());
+
+            if (c1 >= 'A' && c1 <= 'Z')
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
