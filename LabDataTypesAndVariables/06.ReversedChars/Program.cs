a73839bc13049330095bbefc77370736fb691d6b
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-19 19:49:34 +0200

    Added ex7

--- LabDataTypesAndVariables/06.ReversedChars/Program.cs
+++ LabDataTypesAndVariables/06.ReversedChars/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            char c1 = char.Parse(Console.ReadLine());
+            char c2 = char.Parse(Console.ReadLine());
+            char c3 = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{c3} {c2} {c1}");
         }
     }
 }
