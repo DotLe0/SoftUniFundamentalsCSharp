8d7c477531a74d0ed8bfecba92dcda8e3fe3c7e6
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-19 19:42:10 +0200

    Ex 2

---
+++ LabDataTypesAndVariables/02.PoundsToDollars/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _02.PoundsToDollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double paund = double.Parse(Console.ReadLine()) * 1.31;
+            Console.WriteLine($"{paund:F3}");
+        }
+    }
+}
