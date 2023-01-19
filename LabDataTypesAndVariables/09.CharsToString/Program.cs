29c114210b3728f07f5e4f2fda04c119b415c89d
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-19 19:55:29 +0200

    Adding ex8 and ex9

---
+++ LabDataTypesAndVariables/09.CharsToString/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _09.CharsToString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char c1 = char.Parse(Console.ReadLine());
+            char c2 = char.Parse(Console.ReadLine());
+            char c3 = char.Parse(Console.ReadLine());
+
+            string combined = c1.ToString() + c2.ToString() + c3.ToString();
+
+            Console.WriteLine(combined);
+        }
+    }
+}
