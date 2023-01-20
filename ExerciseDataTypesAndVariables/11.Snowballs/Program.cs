5a05b46172e8fd2ae16310bd544bf4803021fe0a
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-20 21:35:48 +0200

    adding ex11

---
+++ ExerciseDataTypesAndVariables/11.Snowballs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Numerics;
+
+namespace _11.Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSnowballs = int.Parse(Console.ReadLine());
+
+            BigInteger maxValue = 0;
+            int bestSnow = 0;
+            int bestTime = 0;
+            int bestQuantity = 0;
+
+            for (int i = 0; i < numberOfSnowballs; i++)
+            {
+                int snow = int.Parse(Console.ReadLine());
+                int time = int.Parse(Console.ReadLine());
+                int quantity = int.Parse(Console.ReadLine());
+
+                BigInteger value = BigInteger.Pow(snow/time, quantity);
+
+                if (value > maxValue)
+                {
+                    maxValue = value;
+                    bestSnow= snow;
+                    bestQuantity= quantity;
+                    bestTime= time;
+                }
+            }
+            Console.WriteLine($"{bestSnow} : {bestTime} = {maxValue} ({bestQuantity})");
+        }
+    }
+}
