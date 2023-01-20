aec4d2e9c8c606001be334d0bec9a096ac8a94d4
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-20 18:52:09 +0200

    adding ex2

--- ExerciseDataTypesAndVariables/01.IntegerOperations/Program.cs
+++ ExerciseDataTypesAndVariables/01.IntegerOperations/Program.cs
@@ -9,7 +9,7 @@
             int num3 = int.Parse(Console.ReadLine());
             int num4 = int.Parse(Console.ReadLine());
 
-            int result = ((num1 + num2) / num3) * num3;
+            int result = ((num1 + num2) / num3) * num4;
             Console.WriteLine(result);
         }
     }
