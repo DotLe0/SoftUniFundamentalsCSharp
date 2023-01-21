aa1d896267f5329533b9bd8ce3439d925395f85f
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-21 13:58:51 +0200

    added ex10

---
+++ ExerciseDataTypesAndVariables/10.PokeMon/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _10.PokeMon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            decimal fiftyPercent = (n * 0.5m);
+            int count = 0;
+            while (n >= m)
+            {
+                n -= m;
+                count++;
+
+                if (n == fiftyPercent && y != 0)
+                {
+                    n /= y;      
+                }               
+            }
+            Console.WriteLine(n);
+            Console.WriteLine(count);
+        }
+    }
+}
