8a81c1fb2704a0d0abff3040fbb73d62b0c730d4
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-21 12:27:53 +0200

    Adding ex5

---
+++ ExerciseDataTypesAndVariables/05.PrintPartOfASCIITable/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _05.PrintPartOfASCIITable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (char i = (char)start; i <= end; i++)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
