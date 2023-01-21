4587a6b329472adabba88623db8e80c70039806c
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-21 12:19:17 +0200

    Added ex3

---
+++ ExerciseDataTypesAndVariables/3.Elevator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _3.Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int elevatorCapsity = int.Parse(Console.ReadLine());
+
+            int courses = (int)Math.Ceiling((double)numberOfPeople / elevatorCapsity);
+
+            Console.WriteLine(courses);
+        }
+    }
+}
