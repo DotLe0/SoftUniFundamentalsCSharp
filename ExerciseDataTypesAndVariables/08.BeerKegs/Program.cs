9d9a76760acf0143af45f5da7d54dd85502578ae
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-21 13:03:23 +0200

    adding ex8

---
+++ ExerciseDataTypesAndVariables/08.BeerKegs/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _08.BeerKegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfKegs = int.Parse(Console.ReadLine());
+            string bestKeg = string.Empty;
+            double bigestVolume = double.MinValue;
+
+            for (int i = 0; i < numberOfKegs; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double result = Math.PI * Math.Pow(radius, 2) * height;
+
+                if (result > bigestVolume)
+                {
+                    bigestVolume = result;
+                    bestKeg= model;
+                }
+            }
+            Console.WriteLine(bestKeg);
+        }
+    }
+}
