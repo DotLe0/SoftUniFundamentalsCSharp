8709d66e54c0baeb4df4d46f069097622e99faf5
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-24 20:10:10 +0200

    Optional project

---
+++ PracticalProjectRockPaperScissors/PracticalProjectRockPaperScissors/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq.Expressions;
+
+namespace PracticalProjectRockPaperScissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chosee [r]ock [p]aper [s]cissors: ");
+            string playerMove = Console.ReadLine();
+
+            string rock = "Rock";
+            string paper = "Paper";
+            string Scissors = "Scissors";
+
+            switch (playerMove)
+            {
+                case "r":
+                    playerMove = rock; break;
+                case "p":
+                    playerMove = paper; break;
+                case "s":
+                    playerMove = Scissors; break;
+                default:
+                    Console.WriteLine("Invalid Input. Try Again...");
+                    break;
+            }
+
+            string[] computerMoves = new string[] { "Rock", "Paper", "Scissors" };  
+            Random rnd = new Random();
+            int index = rnd.Next(0,3);
+
+            string computerMove = computerMoves[index];
+            Console.WriteLine($"The computer chose {computerMove}");
+
+            if ((playerMove == "Rock" && computerMove == "Scissors") || 
+                (playerMove == "Paper" && computerMove == "Rock")    || 
+                (playerMove == "Scissors" && computerMove == "Paper"))
+            {
+                Console.WriteLine("You win.");
+            }
+            else if ((computerMove == "Rock" && playerMove == "Scissors") ||
+                (computerMove == "Paper" && playerMove == "Rock") ||
+                (computerMove == "Scissors" && playerMove == "Paper"))
+            {
+                Console.WriteLine("You lose.");
+            }
+            else
+            {
+                Console.WriteLine("This game was a draw.");
+            }
+        }
+    }
+}
