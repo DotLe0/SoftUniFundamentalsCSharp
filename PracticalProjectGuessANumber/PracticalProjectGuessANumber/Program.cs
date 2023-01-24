7241fb68043d970d3630dc460d87dc8ca14a3f79
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-24 20:21:22 +0200

    Optional project

---
+++ PracticalProjectGuessANumber/PracticalProjectGuessANumber/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace PracticalProjectGuessANumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number (1 - 100):");
+            Random rnd = new Random();
+            int computerNumber = rnd.Next(1, 101);
+
+            while (true)
+            {
+                bool isValid = int.TryParse(Console.ReadLine(), out int playerNumber);
+
+                if (isValid)
+                {
+                    if (playerNumber == computerNumber)
+                    {
+                        Console.WriteLine("You guessed it.");
+                        break;
+                    }
+                    else if (playerNumber < computerNumber)
+                    {
+                        Console.WriteLine("Too low");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too High");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            }
+        }
+    }
+}
