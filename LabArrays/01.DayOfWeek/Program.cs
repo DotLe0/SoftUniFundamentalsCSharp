5f0e3c86ae404e6122d0e3cf78efad18dbcb29f1
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-25 19:21:48 +0200

    Added ex1

---
+++ LabArrays/01.DayOfWeek/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LabArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            int intAsDayOfWeek = int.Parse(Console.ReadLine());
+
+            if (intAsDayOfWeek >=1 && intAsDayOfWeek <=7)
+            {
+                Console.WriteLine(daysOfWeek[intAsDayOfWeek-1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
