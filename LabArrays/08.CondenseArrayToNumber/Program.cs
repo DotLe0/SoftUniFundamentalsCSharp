0982ad35108672417097834bba8ce6a9969661ba
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-25 21:05:51 +0200

    Added ex8

---
+++ LabArrays/08.CondenseArrayToNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp75
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] condensed = new int[nums.Length - 1];
+
+            if (nums.Length == 1)
+            {
+                Console.WriteLine($"{nums[0]}");
+            }
+            else
+            {
+                int n = nums.Length - 1;
+                while (n != 0)
+                {
+                    for (int i = 0; i < nums.Length - 1; i++)
+                    {
+                        condensed[i] = nums[i] + nums[i + 1];
+                    }
+                    nums = condensed;
+                    n--;
+                }
+
+                Console.WriteLine(string.Join(" ", condensed[0]));
+            }        
+        }
+    }
+}
