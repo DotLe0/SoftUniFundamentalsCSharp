43265d04cebb90246476aec29e0c3d8e001087ad
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-26 21:36:26 +0200

    added ex3

---
+++ LabArrays/03.RoundingNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03.RoundingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+                Console.WriteLine($"{array[i]} => {(int)Math.Round(array[i], MidpointRounding.AwayFromZero)}");
+            }                
+        }
+    }
+}
