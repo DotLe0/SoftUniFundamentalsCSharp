4def77e2544ebdb2ffc035de11ed302e69189358
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-27 20:07:46 +0200

    added ex7

---
+++ ExerciseArrays/07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _07.MaxSequenceOfEqualElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int longestSequenceStartIndex = 0;
+            int longestSequenceLenght = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int currentSequanceStart = i - 1;
+                int currentSequenceLenght = 0;
+
+                while (i < array.Length && array[i] == array[i - 1])
+                {
+                    currentSequenceLenght++;
+                    i++;
+                }
+
+                if (currentSequenceLenght > longestSequenceLenght)
+                {
+                    longestSequenceLenght = currentSequenceLenght;
+                    longestSequenceStartIndex = currentSequanceStart;
+                }
+            }
+
+            for (int i = longestSequenceStartIndex; i <= longestSequenceStartIndex + longestSequenceLenght; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+        }
+    }
+}
