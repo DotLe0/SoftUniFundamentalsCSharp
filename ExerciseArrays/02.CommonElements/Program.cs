84daca26d6be107829c1caf3bed27cd6651da632
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-29 19:44:40 +0200

    added ex 2

---
+++ ExerciseArrays/02.CommonElements/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _02.CommonElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = Console.ReadLine().Split();
+            string[] array2 = Console.ReadLine().Split();
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                string currentElement = array2[i];
+
+                if (array1.Contains(currentElement))
+                {
+                    Console.WriteLine(currentElement + " ");
+                }
+            }
+        }
+    }
+}
