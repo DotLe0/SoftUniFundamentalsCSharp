62d2d7cc8e6e58cb6f0f0cae192897aaabbcf79b
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-01-29 20:10:21 +0200

    ex4

---
+++ ExerciseArrays/04.ArrayRotation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Buffers;
+
+namespace ConsoleApp75
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lst = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int k = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= k; i++)
+            {
+                int temp = lst[0];
+                lst.RemoveAt(0);
+                lst.Insert(lst.Count, temp);
+                Console.WriteLine(string.Join(" ", lst));
+            }
+            //Console.WriteLine(string.Join(" ", lst));
+        }
+    }
+}
