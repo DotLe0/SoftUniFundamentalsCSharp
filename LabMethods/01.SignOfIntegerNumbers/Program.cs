22156866edd2a00249a825da7245a5478b06095c
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-02-02 18:45:23 +0200

    added ex1

--- LabMethods/01.SignOfIntegerNumbers/Program.cs
+++ LabMethods/01.SignOfIntegerNumbers/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-
+            int n = int.Parse(Console.ReadLine());
+            SignOfInteger(n);
         }    
+        public static void SignOfInteger(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine($"The number {n} is positive. ");
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine($"The number {n} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {n} is zero. ");
+            }
+        }
     }
 }
