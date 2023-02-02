ec2222a2ffb1850e164d2134d04b8297a95431ae
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-02-02 19:21:48 +0200

    ex 8

---
+++ LabMethods/08.MathPower/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _08.MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum = double.Parse(Console.ReadLine());
+            double secondNum = double.Parse(Console.ReadLine());
+            double result = Power(firstNum, secondNum);
+            Console.WriteLine(result);
+        }
+
+        private static double Power(double firstNum, double secondNum)
+        {
+            return Math.Pow(firstNum, secondNum);
+        }
+    }
+}
