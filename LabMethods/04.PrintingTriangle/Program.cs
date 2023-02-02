603b74f474328eacc5294e8d64463878b528f1da
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-02-02 19:01:21 +0200

    ex 4

---
+++ LabMethods/04.PrintingTriangle/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _04.PrintingTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Triangle(n);
+        }
+        public static void Triangle(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
