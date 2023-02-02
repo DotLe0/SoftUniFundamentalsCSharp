a005a98e39f3154873bb7a8a6627fde65c03a96d
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-02-02 19:11:52 +0200

    ex 6 and ex 7

---
+++ LabMethods/06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _06.CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double hight = double.Parse(Console.ReadLine());
+            double result = GetArea(width, hight);
+            Console.WriteLine(result);
+        }
+
+        private static double GetArea(double width, double hight)
+        {
+            return width * hight;
+        }
+    }
+}
---
+++ LabMethods/07.RepeatString/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _07.RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            string final = RepeatString(text, count);
+            Console.WriteLine(final);
+        }
+
+        private static string RepeatString(string? text, int count)
+        {
+            string temp = string.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                temp += text;
+            }
+            return temp;
+        }
+    }
+}
