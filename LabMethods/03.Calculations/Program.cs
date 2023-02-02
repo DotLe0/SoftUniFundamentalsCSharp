893757ea60d2f7eb927d5a199faf2e2d171400c2
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-02-02 18:54:59 +0200

    ex 2 and ex 3

---
+++ LabMethods/03.Calculations/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _03.Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+
+            switch (command)
+            {
+                case "add":
+                    add(num1, num2); break;
+                case "multiply":
+                    Multiply(num1, num2); break;
+                case "subtract":
+                    Subtract(num1, num2); break;
+                case "divide":
+                    Divide(num1, num2); break;
+            }
+        }
+
+        private static void Divide(double num1, double num2)
+        {
+            Console.WriteLine(num1 / num2);
+        }
+
+        private static void Subtract(double num1, double num2)
+        {
+            Console.WriteLine(num1 - num2);
+        }
+
+        private static void Multiply(double num1, double num2)
+        {
+            Console.WriteLine(num1 * num2);
+        }
+
+        private static void add(double num1, double num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
