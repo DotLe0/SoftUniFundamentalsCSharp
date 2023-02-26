265869a3111679568e0ce12e6e8c93d2460e8133
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-02-26 19:18:10 +0200

    ex 5 and ex 7
    ex 5 bug fixxed

--- ExerciseObjectsАndClasses/05.TeamworkProjects/Program.cs
+++ ExerciseObjectsАndClasses/05.TeamworkProjects/Program.cs
@@ -4,7 +4,96 @@
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Team> teams = new List<Team>();
+
+            for (int i = 0; i < n; i++)
+            {
+                List<string> inputForTeams = Console.ReadLine().Split("-", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                if (teams.Select(x => x.Name).Contains(inputForTeams[1]))
+                {
+                    Console.WriteLine($"Team {inputForTeams[1]} was already created!");
+                    continue;
+                }
+                if (teams.Select(x => x.TeamCreator).Contains(inputForTeams[0]))
+                {
+                    Console.WriteLine($"{inputForTeams[0]} cannot create another team!");
+                    continue;
+                }
+
+                Console.WriteLine($"Team {inputForTeams[1]} has been created by {inputForTeams[0]}!");
+                Team team = new Team(inputForTeams[0], inputForTeams[1]);
+                teams.Add(team);
+            }
             
+            while (true)
+            {
+                string[] inputForMembers = Console.ReadLine().Split("->", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                if (inputForMembers[0] == "end of assignment")
+                {
+                    break;
+                }
+
+                if (!teams.Select(x => x.Name).Contains(inputForMembers[1]))
+                {
+                    Console.WriteLine($"Team {inputForMembers[1]} does not exist!");
+                    continue;
+                }
+
+                if (teams.Select(x => x.TeamCreator).Contains(inputForMembers[0]) 
+                    || teams.SelectMany(x => x.Members).Contains(inputForMembers[0]))
+                {
+                    Console.WriteLine($"Member {inputForMembers[0]} cannot join team {inputForMembers[1]}!");
+                    continue;
+                }
+
+                for (int i = 0; i < teams.Count; i++)
+                {
+                    if (teams[i].Name == inputForMembers[1])
+                    {
+                        teams[i].Members.Add(inputForMembers[0]);
+                    }
+                }
+            }
+
+            var OrderedTeamsByCountAndName = teams.OrderByDescending(x => x.Members.Count).ThenBy(x => x.Name);
+
+            foreach (Team item in OrderedTeamsByCountAndName)
+            {
+                if (item.Members.Count != 0)
+                {
+                    Console.WriteLine(item.Name);
+                    Console.WriteLine("- " + item.TeamCreator);
+                    foreach (string person in item.Members.OrderBy(x=>x))
+                    {
+                        Console.WriteLine("-- " + person);
+                    }
+                }
+            }
+            Console.WriteLine($"Teams to disband:");
+            foreach (Team item in OrderedTeamsByCountAndName)
+            {
+                if (item.Members.Count == 0)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
         }
+    }
+    class Team
+    {
+        public Team(string teamCreator, string name)
+        {
+            TeamCreator = teamCreator;
+            Name = name;
+            Members = new List<string>();
+        }
+
+        public string TeamCreator { get; set; }
+        public string Name { get; set; }
+        public List<string> Members{ get; set; }
     }
 }
---
+++ ExerciseObjectsАndClasses/07.OrderByAge/Program.cs
@@ -0,0 +1,50 @@
+﻿List<Person> lstPersons = new List<Person>();
+
+while (true)
+{
+    List<string> input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+    if (input[0] == "End")
+    {
+        break;
+    }
+
+    if (lstPersons.Select(x => x.Id).Contains(int.Parse(input[1])))
+    {
+        foreach (Person pr in lstPersons)
+        {
+            if (pr.Id == int.Parse(input[1]))
+            {
+                pr.Name = input[0];
+                pr.Age = int.Parse(input[2]);
+            }
+        }
+        continue;
+    }
+
+    Person person = new Person(input[0],int.Parse(input[1]),int.Parse(input[2]));
+    lstPersons.Add(person);
+}
+
+foreach (Person item in lstPersons.OrderBy(x => x.Age))
+{
+    Console.WriteLine(item.ToString());
+}
+
+class Person
+{
+    public Person(string name, int id, int age)
+    {
+        Name = name;
+        Id = id;
+        Age = age;
+    }
+
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public int Age { get; set; }
+    public override string ToString()
+    {
+        return $"{Name} with ID: {Id} is {Age} years old.";
+    }
+}
