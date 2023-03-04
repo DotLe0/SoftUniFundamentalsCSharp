698a6afefaed859b40677c70d90e197f32d53163
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-03-04 13:38:30 +0200

    ex 2 added

---
+++ ExerciseAssociativeArrays/02.AMinerTask/Program.cs
@@ -0,0 +1,25 @@
+﻿Dictionary<string, double> mine = new Dictionary<string, double>();
+
+while (true)
+{
+    string resourse = Console.ReadLine();
+	if (resourse == "stop")
+	{
+		break;
+	}
+
+	double quantity = double.Parse(Console.ReadLine());
+
+	if (!mine.ContainsKey(resourse))
+	{
+		mine.Add(resourse, quantity);
+		continue;
+	}
+
+	mine[resourse] += quantity;
+}
+
+foreach(var kvp in mine)
+{
+    Console.WriteLine($"{kvp.Key} -> {kvp.Value:F2}");
+}
