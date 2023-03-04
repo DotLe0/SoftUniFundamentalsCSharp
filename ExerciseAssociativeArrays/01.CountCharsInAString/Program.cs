02c3041e632c8f9bfd7228c800636db1d369dcff
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-03-04 13:29:41 +0200

    ex 1 added

--- ExerciseAssociativeArrays/01.CountCharsInAString/Program.cs
+++ ExerciseAssociativeArrays/01.CountCharsInAString/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dictionary<char,int> keyValuePairs = new Dictionary<char,int>();
+
+string[] input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+for (int i = 0; i < input.Length; i++)
+{
+	foreach (char item in input[i])
+	{
+		if (!keyValuePairs.ContainsKey(item))
+		{
+			keyValuePairs.Add(item, 1);
+		}
+		else
+		{
+			keyValuePairs[item] += 1;
+		}
+	}
+}
+
+foreach (var item in keyValuePairs)
+{
+    Console.WriteLine($"{item.Key} -> {item.Value}");
+}
