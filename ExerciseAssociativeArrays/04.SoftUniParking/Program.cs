23aa1e233d6c174ab0f0bdec2046f1a99c1f2e5a
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-03-04 19:43:26 +0200

    ex 4

---
+++ ExerciseAssociativeArrays/04.SoftUniParking/Program.cs
@@ -0,0 +1,36 @@
+﻿Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    List<string> input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).ToList();
+
+	if (input[0] == "register")
+	{
+		if (!keyValuePairs.ContainsKey(input[1]))
+		{
+			keyValuePairs.Add(input[1], input[2]);
+            Console.WriteLine($"{input[1]} registered {input[2]} successfully");
+			continue;
+        }
+
+        Console.WriteLine($"ERROR: already registered with plate number {input[2]}");
+    }
+	else if (input[0] == "unregister")
+	{
+		if (!keyValuePairs.ContainsKey(input[1]))
+		{
+            Console.WriteLine($"ERROR: user {input[1]} not found");
+			continue;
+        }
+
+		keyValuePairs.Remove(input[1]);
+		Console.WriteLine($"{input[1]} unregistered successfully");
+    }
+}
+
+foreach (var item in keyValuePairs)
+{
+    Console.WriteLine($"{item.Key} => {item.Value}");
+}
