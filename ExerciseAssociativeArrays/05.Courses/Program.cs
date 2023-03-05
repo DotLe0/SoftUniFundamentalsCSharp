bbb6d77c5515e29016b9af67d6bfd7423aee7a4a
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-03-05 12:46:29 +0200

    ex 5 and 6

---
+++ ExerciseAssociativeArrays/05.Courses/Program.cs
@@ -0,0 +1,25 @@
+﻿Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+while (true)
+{
+    List<string> list = Console.ReadLine().Split(":", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+    if (list[0] == "end")
+    {
+        break;
+    }
+
+    if (!dict.ContainsKey(list[0].Trim()))
+    {
+        dict.Add(list[0].Trim(), new List<string> { list[1] });
+        continue;
+    }
+
+    dict[list[0].Trim()].Add(list[1]);
+}
+
+foreach (var kvp in dict)
+{
+    Console.WriteLine($"{kvp.Key}: {kvp.Value.Count}\n--{string.Join("\n--", kvp.Value)}");
+}
+
---
+++ ExerciseAssociativeArrays/06.StudentAcademy/Program.cs
@@ -0,0 +1,25 @@
+﻿Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string name = Console.ReadLine();
+    double grade = double.Parse(Console.ReadLine());
+
+    if (!students.ContainsKey(name))
+    {
+        students.Add(name, new List<double>());
+        students[name].Add(grade);
+        continue;
+    }
+
+    students[name].Add(grade);
+}
+
+var filteredStudents = students.Where(x => x.Value.Average() >= 4.50);
+
+foreach (var student in filteredStudents)
+{
+    Console.WriteLine($"{student.Key} -> {student.Value.Average():F2}");
+}
