2540a140659c3b63dc704b426785adf6ad0fbce6
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-03-09 22:25:36 +0200

    ex 5

--- TextProcessingLab/05.DigitsLettersAndOther/Program.cs
+++ TextProcessingLab/05.DigitsLettersAndOther/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string input = Console.ReadLine();
+
+List<char> letters = new List<char>();
+List<char> digits = new List<char>();
+List<char> symbols = new List<char>();
+
+for (int i = 0; i < input.Length; i++)
+{
+	if (char.IsLetter(input[i]))
+	{
+		letters.Add(input[i]);
+	}
+	else if (char.IsDigit(input[i])) 
+	{
+		digits.Add(input[i]); 
+	}
+	else if (input[i] >= 33 && input[i] <= 64)
+	{
+		symbols.Add(input[i]);
+	}
+	else if (char.IsSymbol(input[i]))
+	{
+		symbols.Add(input[i]);
+	}
+}
+
+Console.WriteLine(string.Join("", digits));
+Console.WriteLine(string.Join("", letters));
+Console.WriteLine(string.Join("", symbols));
